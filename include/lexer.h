689bce8be8f3ed6761a0991d9129b4c1987a660e
amyyrodri <yguuuto@gmail.com>
2025-11-25 19:34:16 -0300

    feat: header lexer > struct

---
+++ include/lexer.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 19:30:19 by kamys             #+#    #+#             */
+/*   Updated: 2025/11/25 19:33:17 by kamys            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+typedef enum s_token_type
+{
+	TK_WORD,
+	TK_PIPE,
+	TK_AND,
+	TK_OR,
+	TK_LIST,
+	TK_LPAREN,
+	TK_RPAREN,
+	TK_REDIR_IN,
+	TK_REDIR_OUT,
+	TK_HEREDOC,
+	TK_APPEND,
+	TK_EOF
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+}	t_token;
+
+
+#endif
