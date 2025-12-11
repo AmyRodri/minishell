03ac0600e139d2247da68c8764c2daa3fb26eb0d
amy rodrigues <rmmgustavo@gmail.com>
2025-12-11 04:42:58 -0300

    fix: norminette

--- srcs/parser/parse_command.c
+++ srcs/parser/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 03:15:16 by amyrodri          #+#    #+#             */
-/*   Updated: 2025/12/11 03:31:36 by amyrodri         ###   ########.fr       */
+/*   Updated: 2025/12/11 04:42:00 by amyrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,64 +21,80 @@ t_redir_type	redir_type(t_token_type type)
 	return (REDIR_APPEND);
 }
 
+t_redir	*redirs(t_token **tokens, t_token_type type)
+{
+	t_redir			*redir;
+	t_redir_type	rtype;
+	t_redir			*r;
+	redir = NULL;
+	rtype = redir_type(type);
+	*tokens = (*tokens)->next;
+	if (!*tokens || (*tokens)->type != TK_WORD)
+	{
+		printf("Error: filename expected\n");
+		exit (1);
+	}
+	r = malloc(sizeof(t_redir));
+	r->type = rtype;
+	r->file = ft_strdup((*tokens)->value);
+	r->next = redir;
+	redir = r;
+	*tokens = (*tokens)->next;
+	return (redir);
+}
 
+void	add_word_arg(t_cmd *cmd, t_token **tokens)
+{
+	cmd->argv = realloc(cmd->argv, sizeof(char*) * (cmd->argc + 2)); // realloc slk pq {Ö.Ö}
+	cmd->argv[cmd->argc] = ft_strdup((*tokens)->value);
+	cmd->argv[cmd->argc + 1] = NULL;
+	cmd->argc++;
+	*tokens = (*tokens)->next;
+}
+
+int	is_stop_token(t_token_type type)
+{
+	return (type == TK_PIPE || type == TK_AND || type == TK_OR
+		|| type == TK_LIST || type == TK_RPAREN);
+}
+
+t_ast	*new_node(t_node_type type, t_ast *left, t_ast *right, void *content)
+{
+	t_ast	*node;
+	
+	node = malloc(sizeof(t_ast));
+	node->type = type;
+	node->left = left;
+	node->right = right;
+	node->content = content;
+	return (node);
+}
 
 t_ast	*parse_command(t_token **tokens)
 {
-	t_redir	*redir;
-	t_cmd	*cmd;
-	char	**argv;
-	int		argc;
-
+	t_token_type	type;
+	t_redir			*redir;
+	t_cmd			*cmd;
+	
 	redir = NULL;
-	argv = NULL;
-	argc = 0;
 	cmd = ft_calloc(1, sizeof(t_cmd));
 	while (*tokens)
 	{
-		t_token_type	type = (*tokens)->type;
-		if (type == TK_PIPE || type == TK_AND || type == TK_OR
-			|| type == TK_LIST || type == TK_RPAREN)
+		type = (*tokens)->type;
+		if (is_stop_token(type))
 			break ;
-
 		if (type == TK_REDIR_IN || type == TK_REDIR_OUT || type == TK_APPEND)
 		{
-			t_redir_type	rtype = redir_type(type);
-			*tokens = (*tokens)->next;
-			if (!*tokens || (*tokens)->type != TK_WORD)
-			{
-				printf("Error: filename expected\n");
-				exit (1);
-			}
-			t_redir *r = malloc(sizeof(t_redir));
-			r->type = rtype;
-			r->file = ft_strdup((*tokens)->value);
-			r->next = redir;
-			redir = r;
-			*tokens = (*tokens)->next;
+			redir = redirs(tokens, type);
 			continue ;
 		}
-
 		if (type == TK_WORD)
 		{
-			argv = realloc(argv, sizeof(char*) * (argc + 2)); // realloc slk pq {Ö.Ö}
-			argv[argc] = ft_strdup((*tokens)->value);
-			argv[argc + 1] = NULL;
-			argc++;
-			*tokens = (*tokens)->next;
+			add_word_arg(cmd, tokens);
 			continue ;
 		}
 		break ;
 	}
-
-	cmd->argv = argv;
-	cmd->argc = argc;
 	cmd->redir = redir;
-
-	t_ast	*node = malloc(sizeof(t_ast));
-	node->type = NODE_CMD;
-	node->left = NULL;
-	node->right = NULL;
-	node->content = cmd;
-	return (node);
+	return (new_node(NODE_CMD, NULL, NULL, cmd));
 }
