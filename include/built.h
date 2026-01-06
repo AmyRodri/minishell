0ecfbbc9cb2caea5f5466ebb75743e9acea4fc21
amyyrodri <yguuuto@gmail.com>
2026-01-06 16:39:03 -0300

    fix: norminette organization

--- include/built.h
+++ include/built.h
@@ -6,7 +6,7 @@
 /*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 13:00:29 by amyrodri          #+#    #+#             */
-/*   Updated: 2026/01/22 11:35:20 by kamys            ###   ########.fr       */
+/*   Updated: 2026/01/22 11:36:19 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@
 typedef struct s_env_table	t_env_table;
 typedef struct s_cmd		t_cmd;
 
-/* ========== EXPORT UTILS ========== */
-
+// ==================== EXPORT ======================
 char	*ft_strjoin3(char *s1, char *s2, char *s3);
+void	display_export(t_env_table *env);
 int		env_len(t_env_table *table);
 int		is_append(char *s);
+// ==================== EXPORT ======================
 
 /* ========== BUILT OPERATIONS ========== */
 
