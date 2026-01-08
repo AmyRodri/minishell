d0b06e8222c685ea8b2347d26a81f87d7bd60274
amyyrodri <yguuuto@gmail.com>
2026-01-08 15:43:05 -0300

    fix: leaks

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 13:17:23 by kamys             #+#    #+#             */
-/*   Updated: 2026/01/13 16:12:50 by amyrodri         ###   ########.fr       */
+/*   Updated: 2026/01/13 16:14:28 by amyrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,63 @@ void	exec_built_in(t_env_table *env, t_cmd *cmd)
 		return (unset(env, cmd));
 }
 
+void	free_tokens(t_token *tok)
+{
+	t_token	*tmp;
+
+	while (tok)
+	{
+		tmp = tok->next;
+		free(tok->value);
+		free(tok);
+		tok = tmp;
+	}
+}
+
+void	free_redir(t_redir *r)
+{
+	t_redir *next;
+
+	while (r)
+	{
+		next = r->next;
+		free(r->file);
+		free(r);
+		r = next;
+	}
+}
+
+void	free_cmd(t_cmd *cmd)
+{
+	int	i;
+
+	if (!cmd)
+		return ;
+	if (cmd->argv)
+	{
+		i = 0;
+		while (cmd->argv[i])
+			free(cmd->argv[i++]);
+		free(cmd->argv);
+	}
+	free_redir(cmd->redir);
+	free(cmd);
+}
+
+void	free_ast(t_ast *root)
+{
+	if (!root)
+		return ;
+	free_ast(root->left);
+	free_ast(root->right);
+	if (root->type == NODE_CMD)
+		free_cmd((t_cmd *)root->content);
+	else
+		free(root->content);
+	free(root);
+}
+
+
 void	input(char	*line, t_env_table	*env)
 {
 	t_token	*token;
@@ -41,6 +98,8 @@ void	input(char	*line, t_env_table	*env)
 	if (ast_root)
 		exit_status = execute_ast(ast_root, env);
 	env_set(env, "LAST_CODE", ft_itoa(exit_status));
+	free_ast(ast_root);
+	free_tokens(token);
 }
 
 int	run_interactive_shell(t_env_table *env)
