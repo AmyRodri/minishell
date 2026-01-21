4a2cbe376051e4a3c6291cb17ba98155621711ab
Amy Rodrigues <rmmgustavo@gmail.com>
2026-01-21 16:49:44 -0300

    fix: time for segund command

--- srcs/exec/exec_pipe.c
+++ srcs/exec/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 12:26:40 by cassunca          #+#    #+#             */
-/*   Updated: 2026/01/14 15:36:43 by amyrodri         ###   ########.fr       */
+/*   Updated: 2026/01/21 16:44:27 by amyrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,27 @@ static void	close_fd(int fd1, int fd2)
 int	handle_pipe(t_ast *root, t_shell *sh)
 {
 	int		fd[2];
-	pid_t	pid;
+	pid_t	pid[2];
 	int		status;
 
 	if (pipe(fd) == -1)
 		return (1);
-	if (fork() == 0)
+	pid[0] = fork();
+	if (pid[0] == 0)
 	{
 		dup2(fd[1], STDOUT_FILENO);
 		close_fd(fd[0], fd[1]);
 		exit(execute_ast(root->left, sh));
 	}
-	pid = fork();
-	if (pid == 0)
+	waitpid(pid[0], &status, 0);
+	pid[1] = fork();
+	if (pid[1] == 0)
 	{
 		dup2(fd[0], STDIN_FILENO);
 		close_fd(fd[0], fd[1]);
 		exit(execute_ast(root->right, sh));
 	}
 	close_fd(fd[0], fd[1]);
-	waitpid(-1, NULL, 0);
-	waitpid(pid, &status, 0);
+	waitpid(pid[1], &status, 0);
 	return (WEXITSTATUS(status));
 }
