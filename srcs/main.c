e0a389865764ce10c16b7c2fe0cb84d4a9e53589
amyyrodri <yguuuto@gmail.com>
2026-02-01 13:32:28 -0300

    fix: remove comment

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 13:17:23 by kamys             #+#    #+#             */
-/*   Updated: 2026/01/27 14:54:33 by amyrodri         ###   ########.fr       */
+/*   Updated: 2026/02/01 13:31:47 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ static void	input(char	*line, t_shell *sh)
 	ast_root = parser(token);
 	free_tokens(token);
 	expand_alias_ast(ast_root, sh);
-	// expand_ast(ast_root, sh);
 	if (traverse_ast_heredoc(ast_root, sh) == INTERRUPTED_BY_SIGINT)
 	{
 		free_ast(ast_root);
