0913eeb072c6a51eb4568a9d83ba714710b50a82
amyyrodri <yguuuto@gmail.com>
2026-02-03 17:14:19 -0300

    fix: overflow and underflow and negative numbers

--- srcs/built_in/built_exit.c
+++ srcs/built_in/built_exit.c
@@ -6,32 +6,65 @@
 /*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 12:58:03 by amyrodri          #+#    #+#             */
-/*   Updated: 2026/02/01 14:25:18 by kamys            ###   ########.fr       */
+/*   Updated: 2026/02/03 17:12:40 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built.h"
 
-static int	ft_is_numeric(const char *s)
+#include <limits.h>
+
+static int	ft_atoll_safe(const char *s, long long *out)
 {
+	long long	res = 0;
+	int			sign = 1;
+
+	if (*s == '+' || *s == '-')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	if (!*s)
+		return (0);
 	while (*s)
 	{
 		if (!ft_isdigit(*s))
 			return (0);
+
+		if (res > (LLONG_MAX - (*s - '0')) / 10)
+			return (0);
+
+		res = res * 10 + (*s - '0');
 		s++;
 	}
+	*out = res * sign;
 	return (1);
+}
+
+
+
+static int	normalize_exit_code(long long n)
+{
+	int	code;
+
+	code = n % 256;
+	if (code < 0)
+		code += 256;
+	return (code);
 }
 
 int	ft_exit(t_shell *sh, t_cmd *cmd)
 {
+	long long	val;
+
 	if (cmd->argc == 1)
 	{
 		sh->exit_code = sh->last_status;
 		sh->should_exit = 1;
 		return (0);
 	}
-	if (!ft_is_numeric(cmd->argv[1]))
+	if (!ft_atoll_safe(cmd->argv[1], &val))
 	{
 		ft_putendl_fd("Minishell: exit: numeric argument required",
 			STDERR_FILENO);
@@ -45,7 +78,7 @@ int	ft_exit(t_shell *sh, t_cmd *cmd)
 			STDERR_FILENO);
 		return (1);
 	}
-	sh->exit_code = ft_atoi(cmd->argv[1]) % 256;
+	sh->exit_code = normalize_exit_code(val);
 	sh->should_exit = 1;
 	return (sh->exit_code);
 }
